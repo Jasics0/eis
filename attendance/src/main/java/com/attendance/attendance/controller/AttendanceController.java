62c5df15dcd98a6aa76710804cc54c438ff03400
JavierCharry <javier.charry@marvut.com>
2023-09-23 15:25:17 -0500

    Feat: nginex :)

--- attendance/src/main/java/com/attendance/attendance/controller/AttendanceController.java
+++ attendance/src/main/java/com/attendance/attendance/controller/AttendanceController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/attendance")
+@RequestMapping()
 @RequiredArgsConstructor
 public class AttendanceController {
 
